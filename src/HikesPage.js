825dc5b2d2f6d60069d9fbf7c2c585fe2ed94a8b
nataliedelaat <ndelaa@uw.edu>
2024-06-02 15:38:14 -0700

    taking out some comments

--- src/HikesPage.js
+++ src/HikesPage.js
@@ -26,35 +26,3 @@ const HikesPage = () => {
 }
 
 export default HikesPage;
-
-//this is the suggestions about how to alter this page in order to take the user
-//to the desired hike with simply clicking on the card. didn't want to edit your code without asking!
-
-// import React from 'react';
-// import { useHistory } from 'react-router-dom'; // Import useHistory from react-router-dom
-// import hikes from './data/hikes.json'; // Assuming your JSON data is correctly imported
-
-// const HikesPage = () => {
-//     const history = useHistory();
-
-//     // Function to handle click event
-//     const handleCardClick = (hikeId) => {
-//         // Redirect to the individual hike page
-//         history.push(`/hike/${hikeId}`); // Assuming '/hike/${hikeId}' is the URL format for individual hike pages
-//     };
-
-//     return (
-//         <div className="card-container">
-//             {hikes.map((hike) => (
-//                 // Add onClick event handler to each card
-//                 <div className="card" key={hike.id} onClick={() => handleCardClick(hike.id)}> //changing this part
-//                     <img src={`/img/${hike.imageName}`} alt={hike.name} />
-//                     <h2>{hike.name}</h2>
-//                     <p>📍{hike.location}</p>
-//                 </div>
-//             ))}
-//         </div>
-//     );
-// };
-
-// export default HikesPage;
