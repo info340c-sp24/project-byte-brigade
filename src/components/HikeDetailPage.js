816d0ae288d58029f76945c9c673ecd2dd60ae5f
mykyho <mykyho02@gmail.com>
2024-06-03 16:15:09 -0700

    changed the alt for image in detailpage

--- src/components/HikeDetailPage.js
+++ src/components/HikeDetailPage.js
@@ -21,7 +21,7 @@ const HikeDetailPage = () => {
 
             <main>
                 <div className="hike-info">
-                    <img src={`/img/${hike.imageName}`} alt={`Picture of ${hike.name}`} />
+                    <img src={`/img/${hike.imageName}`} alt={`this hike is of /img/${hike.imageName}`} />
                     <h2>About the hike:</h2>
                     <p>Scenery: {hike.scenery}</p>
                     <p>{hike.details} || "No details available!" </p>
