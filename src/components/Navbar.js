6cc0a821dd825982bdca7c8825c1e0e8d185d27b
cinnammj <cinnammj@uw.edu>
2024-06-03 17:00:12 -0700

    navbar fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,9 @@ import React from 'react'; //import React library
 function Navbar({ onLogout }) {
     return (
         <header>
-            
             <div className="topnav">
-                <a class="active" href="#home">Home</a>
-                <a href="#quiz">Quiz</a>
+                <a href="/">Home</a>
+                <a href="/quiz">Quiz</a>
                 <button onClick={onLogout} className="nav-link">Logout</button>
             </div>
         </header>
